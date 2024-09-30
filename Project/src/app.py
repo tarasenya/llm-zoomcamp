163109a54e36c3610400acda8e253dc1a136e9d1
Taras <tarasenya@gmail.com>
2024-09-30 22:03:56 +0200

    Changed print_log to loguru

--- Project/src/app.py
+++ Project/src/app.py
@@ -1,6 +1,8 @@
+import sys
 import streamlit as st
 import time
 import uuid
+from loguru import logger
 
 from ambiguity_resolver_rag import ambiguity_resolver_rag
 from phi_rag import phi3_rag
@@ -11,14 +13,16 @@ from database_operations.db import (
     get_feedback_stats,
 )
 
+logger.remove()
 
-def print_log(message):
-    print(message, flush=True)
+logger.add(
+    sys.stderr, format="{time} | {level} | {message}", level="INFO", enqueue=True
+)
 
 
 def main():
     judge_llm = JudgeLLM()
-    print_log("Starting the vague interpretator")
+    logger.info("Starting the vague interpretator")
     st.title("Vague Intepretator")
 
     # Session state initialization
@@ -26,15 +30,14 @@ def main():
         st.session_state.conversation_id = None
     if "count" not in st.session_state:
         st.session_state.count = 0
-        print_log("Feedback count initialized to 0")
+        logger.info("Feedback count initialized to 0")
 
     # Model selection
     model_choice = st.selectbox(
-        'Select a model:',
-        ['openai/gpt-4o-mini',
-         'ollama/phi3'],
+        "Select a model:",
+        ["openai/gpt-4o-mini", "ollama/phi3"],
     )
-    print_log(f"User selected model: {model_choice}")
+    logger.info(f"User selected model: {model_choice}")
 
     # User input
     user_input = st.text_input("Enter a vague statement from your boss:")
@@ -42,18 +45,18 @@ def main():
     if st.button("Ask"):
         # Generate a new conversation ID when asking a question
         st.session_state.conversation_id = str(uuid.uuid4())
-        print_log(
+        logger.info(
             f"New conversation started with ID: {st.session_state.conversation_id}"
         )
 
-        print_log(f"User asked: '{user_input}'")
+        logger.info(f"User asked: '{user_input}'")
         with st.spinner("Processing..."):
-            print_log(f"Getting answer from assistant using {model_choice}.")
+            logger.info(f"Getting answer from assistant using {model_choice}.")
             start_time = time.time()
-            if model_choice == 'openai/gpt-4o-mini':
+            if model_choice == "openai/gpt-4o-mini":
                 _answer = ambiguity_resolver_rag.rag_results(vague=user_input)
             else:
-                _answer = phi3_rag.rag_results(vague=user_input)    
+                _answer = phi3_rag.rag_results(vague=user_input)
             _judge_llm_input: JudgeLLMPromptInput = {
                 "vague": user_input,
                 "translation": _answer,
@@ -61,14 +64,13 @@ def main():
             _llm_judgement_score: LLMJudgementScore = judge_llm.judget_it(
                 _judge_llm_input
             )
-            print_log(_llm_judgement_score)
             end_time = time.time()
-            print_log(f"Answer received in {end_time - start_time:.2f} seconds")
+            logger.info(f"Answer received in {end_time - start_time:.2f} seconds")
             st.success("Completed!")
             st.write(_answer)
 
             # Save conversation to database
-            print_log("Saving conversation to database")
+            logger.info("Saving conversation to database")
             save_conversation(
                 conversation_id=st.session_state.conversation_id,
                 question=user_input,
@@ -78,7 +80,7 @@ def main():
                 response_time=round(end_time - start_time, 2),
                 timestamp=None,
             )
-            print_log("Conversation saved successfully")
+            logger.info("Conversation has been saved successfully.")
 
     # Feedback buttons
     col1, col2 = st.columns(2)
@@ -86,11 +88,11 @@ def main():
         if st.button("+1"):
             if st.session_state.conversation_id:
                 st.session_state.count += 1
-                print_log(
+                logger.info(
                     f"Positive feedback received. New count: {st.session_state.count}"
                 )
                 save_feedback(st.session_state.conversation_id, 1)
-                print_log("Positive feedback saved to database")
+                logger.info("Positive feedback saved to database")
             else:
                 st.error("Please ask a question before providing feedback.")
 
@@ -98,11 +100,11 @@ def main():
         if st.button("-1"):
             if st.session_state.conversation_id:
                 st.session_state.count -= 1
-                print_log(
+                logger.info(
                     f"Negative feedback received. New count: {st.session_state.count}"
                 )
                 save_feedback(st.session_state.conversation_id, -1)
-                print_log("Negative feedback saved to database")
+                logger.info("Negative feedback saved to database")
             else:
                 st.error("Please ask a question before providing feedback.")
 
@@ -114,9 +116,9 @@ def main():
     st.write(f"Thumbs up: {feedback_stats['thumbs_up']}")
     st.write(f"Thumbs down: {feedback_stats['thumbs_down']}")
 
-    print_log("Streamlit app loop completed")
+    logger.info("Streamlit app loop completed")
 
 
 if __name__ == "__main__":
-    print_log("Vague Translator application started")
+    logger.info("Vague Translator application started")
     main()
